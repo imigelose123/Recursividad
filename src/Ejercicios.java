066ffcca435fe091c639a54a8575f2c097581bb3
Jose Miguel Ascuy Anturiano <imigelose2@gmail.com>
2022-05-30 11:47:03 -0400

    commit recursividad

---
+++ src/Ejercicios.java
@@ -0,0 +1,17 @@
+public class Ejercicios {
+
+        public static int factorial(int n){
+            int factorial;
+
+            if (n == 1){
+                factorial = n;
+            }else {
+                factorial = n * factorial(n - 1);
+            }
+
+
+
+            return  factorial;
+        }
+    }
+
